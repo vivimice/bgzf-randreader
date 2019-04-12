feba1f070d953d5c1240e69ad52aa0af819d3264
vivimice <vivimice@gmail.com>
2019-04-12 17:56:44 +0800

    Add javadoc

--- src/main/java/com/vivimice/bgzfrandreader/RandomAccessBgzFile.java
+++ src/main/java/com/vivimice/bgzfrandreader/RandomAccessBgzFile.java
@@ -1,12 +1,15 @@
 package com.vivimice.bgzfrandreader;
 
+import java.io.Closeable;
+import java.io.EOFException;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
+import java.nio.channels.SeekableByteChannel;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map.Entry;
 import java.util.NavigableMap;
@@ -14,33 +17,115 @@ import java.util.TreeMap;
 import java.util.zip.DataFormatException;
 import java.util.zip.Inflater;
 
-public class RandomAccessBgzFile implements AutoCloseable, Cloneable {
+/**
+ * <p>A Random Access BGZF Reader</p>
+ * 
+ * <p>This class can be used to read uncompressed data of BGZF file at specified offset 
+ * relative to data before compression.</p>
+ * 
+ * <p>BGZF is a GZip compatible file format, it stores block size and input size in extra subfield of each
+ * data block, makes random accessing by uncompressed data offset possible. BGZF file can be created by 
+ * <code>bgzip</code> command line tool from either uncompressed file or existing .gz file.</p>
+ * 
+ * <p>Note: Make sure {@link #close()} is called after use. {@link RandomAccessBgzFile} uses {@link Inflater} 
+ * to decompress data, which must be closed explicitly. Otherwise unexpected off-heap memory leak may occur.</p>
+ * 
+ * <p><b>WARNING: This class is not thread safe.</b> Use with caution when shared with multiple threads.</p>
+ * 
+ * @author vivimice &lt;vivimice@gmail.com&gt;
+ * @see http://samtools.github.io/hts-specs/SAMv1.pdf
+ */
+public class RandomAccessBgzFile implements Closeable, AutoCloseable {
 
-    private final FileChannel channel;
+    private final boolean closeChannelOnClose;
+    private final SeekableByteChannel channel;
     private final NavigableMap<Long, BgzipBlock> blockTree;
     private final long inputLength;
+    private final long basePosition;
     private final Inflater inflater = new Inflater(true);
     
     private long pos = 0;
     
-    public RandomAccessBgzFile(File file) throws IOException {
-        this(new RandomAccessFile(file, "r"));
+    /**
+     * <p>Constructs a RandomAccessBgzFile instance using existing {@link RandomAccessFile}.</p>
+     * 
+     * <p>Note: All following read/seek operations will starts from <code>file</code>'s 
+     * current position. Reading or seeking this RandomAccessBgzFile will affect <code>file</code>'s 
+     * position, and vice versa.</p>
+     * 
+     * <p>Note: <code>file</code> won't be closed when calling {@link #close()} method.</p>
+     * 
+     * @param file
+     * @throws IOException If IO error occurs while building index
+     * @throws NullPointerException If <code>file</code> is <code>null</code>
+     * @throws MalformedBgzipDataException If compressed data is not valid BGZF format
+     */
+    public RandomAccessBgzFile(RandomAccessFile file) 
+            throws IOException, MalformedBgzipDataException {
+        this(file.getChannel(), false);
     }
     
-    private RandomAccessBgzFile(
-            FileChannel channel,
-            NavigableMap<Long, BgzipBlock> blockTree,
-            long inputLength) {
-        this.channel = channel;
-        this.blockTree = blockTree;
-        this.inputLength = inputLength;
+    /**
+     * <p>Constructs a RandomAccessBgzFile instance using existing {@link File}.</p>
+     * 
+     * @param file
+     * @throws IOException If IO error occurs while building index
+     * @throws NullPointerException If <code>file</code> is <code>null</code>
+     * @throws MalformedBgzipDataException If compressed data is not valid BGZF format
+     * @throws FileNotFoundException If <code>file</code> is not a valid file
+     * @throws SecurityException If a security manager exists and its checkRead method denies read access to the file.
+     */
+    @SuppressWarnings("resource")
+    public RandomAccessBgzFile(File file) 
+            throws IOException, FileNotFoundException, MalformedBgzipDataException {
+        this(new FileInputStream(file).getChannel(), true);
     }
     
-    public RandomAccessBgzFile(RandomAccessFile file) throws IOException {
-        if (file == null) {
+    /**
+     * <p>Constructs a RandomAccessBgzFile instance using existing {@link FileInputStream}.</p>
+     * 
+     * <p>Note: All following read/seek operations will starts from the number of 
+     * bytes read from the file so far. Reading or seeking from this RandomAccessBgzFile will
+     * change the stream's position either.</p>
+     * 
+     * <p>Note: <code>in</code> won't be closed when calling {@link #close()} method.</p>
+     * 
+     * @param in
+     * @throws IOException If IO error occurs while building index
+     * @throws NullPointerException If <code>in</code> is <code>null</code>
+     * @throws MalformedBgzipDataException If compressed data is not valid BGZF format
+     */
+    public RandomAccessBgzFile(FileInputStream in) 
+            throws IOException, MalformedBgzipDataException {
+        this(in.getChannel(), false);
+    }
+    
+    /**
+     * <p>Constructs a RandomAccessBgzFile instance using existing {@link SeekableByteChannel}.</p>
+     * 
+     * <p>Note: All following read/seek operations will starts from <code>channel</code>'s 
+     * current position. Reading or seeking this RandomAccessBgzFile will affect <code>channel</code>'s 
+     * position, and vice versa.</p>
+     * 
+     * <p>Note: <code>channel</code> won't be closed when calling {@link #close()} method.</p>
+     * 
+     * @param channel
+     * @throws IOException If IO error occurs while building index
+     * @throws NullPointerException If <code>channel</code> is <code>null</code>
+     * @throws MalformedBgzipDataException If compressed data is not valid BGZF format
+     */
+    public RandomAccessBgzFile(SeekableByteChannel channel) 
+            throws IOException, MalformedBgzipDataException {
+        this(channel, false);
+    }
+    
+    private RandomAccessBgzFile(SeekableByteChannel channel, boolean closeChannelOnClose) 
+            throws IOException, MalformedBgzipDataException {
+        if (channel == null) {
             throw new NullPointerException();
         }
-        this.channel = file.getChannel();
+        this.channel = channel;
+        this.basePosition = channel.position();
         
         // Build index
         NavigableMap<Long, BgzipBlock> blockTree = new TreeMap<>();
@@ -55,21 +140,39 @@ public class RandomAccessBgzFile implements AutoCloseable, Cloneable {
             }
         }
         
-        this.blockTree = Collections.unmodifiableNavigableMap(blockTree);
+        this.blockTree = blockTree;
         this.inputLength = inputOffset;
+        this.closeChannelOnClose = closeChannelOnClose;
     }
     
-    @Override
-    public RandomAccessBgzFile clone() {
-        return new RandomAccessBgzFile(channel, blockTree, inputLength);
-    }
-    
+    /**
+     * <p>Close this {@link RandomAccessBgzFile}</p>
+     * 
+     * <p>Once this method is called, the behavior of the {@link RandomAccessBgzFile} 
+     * object is undefined.</p>
+     * 
+     * <p>Make sure this method is called when finish using {@link RandomAccessBgzFile}
+     * object. Otherwise off-heap memory leak may occur.</p>
+     * 
+     * <p>Note: This method won't close underlying stream / random access file.</p>
+     * 
+     * @throws IOException If I/O error occurs
+     */
     @Override
     public void close() throws IOException {
-        channel.close();
+        if (closeChannelOnClose) {
+            channel.close();
+        }
         inflater.end();
     }
     
+    /**
+     * <p>Sets this {@link RandomAccessBgzFile}'s file position, relative to uncompressed data</p>
+     * 
+     * @param pos
+     * @throws IOException If pos is negative or out of uncompressed data's length
+     * @see {@link #inputLength()}
+     */
     public void seek(long pos) throws IOException {
         if (pos < 0 || pos > inputLength) {
             throw new IOException(String.format("Position %d out of range", pos));
@@ -77,10 +180,30 @@ public class RandomAccessBgzFile implements AutoCloseable, Cloneable {
         this.pos = pos;
     }
     
+    /**
+     * <p>Gets this {@link RandomAccessBgzFile}'s file position, 
+     * relative to uncompressed data</p>
+     * 
+     * @return This {@link RandomAccessBgzFile}'s file position
+     */
     public long getPosition() {
         return pos;
     }
     
+    /**
+     * <p>Attempts to skip over n bytes of input discarding the skipped bytes, 
+     * relative to uncompressed data.</p>
+     * 
+     * <p>This method may skip over some smaller number of bytes, possibly zero. 
+     * This may result from any of a number of conditions; reaching end of file 
+     * before n bytes have been skipped is only one possibility. This method 
+     * never throws an {@link EOFException}. The actual number of bytes skipped is returned. 
+     * If n is negative, no bytes are skipped.</p>
+     * 
+     * @param n
+     * @return The actual number of bytes skipped
+     * @throws IOException If I/O error occurs
+     */
     public int skipBytes(int n) throws IOException {
         if (n > 0) {
             long newPos = pos + n > inputLength ? inputLength : pos + n;
@@ -92,14 +215,47 @@ public class RandomAccessBgzFile implements AutoCloseable, Cloneable {
         }
     }
     
+    /**
+     * <p>Gets uncompressed data length</p>
+     * 
+     * @return Total uncompressed data length
+     */
     public long inputLength() {
         return inputLength;
     }
     
+    /**
+     * <p>Read up to <code>b.length</code> bytes of uncompressed data from this {@link RandomAccessBgzFile} 
+     * into <code>b</code></p>
+     * 
+     * <p>This method will block until at least one byte of input is available.</p>
+     * 
+     * @param b The buffer into which the data will read
+     * @return The total number of bytes read into the buffer, 
+     *         or -1 if there is no more data because the end of this file has been reached. 
+     * @throws IOException If I/O error occurs
+     * @throws NullPointerException If <code>b</code> is <code>null</code>
+     * @throws MalformedBgzipDataException If compressed data is not valid BGZF format
+     */
     public int read(byte[] b) throws IOException {
         return read(b, 0, b.length);
     }
 
+    /**
+     * <p>Read up to <code>len</code> bytes of uncompressed data from this {@link RandomAccessBgzFile} 
+     * into <code>b</code> starting from <code>off</code></p>
+     * 
+     * <p>This method will block until at least one byte of input is available.</p>
+     * 
+     * @param b The buffer into which the data will read
+     * @param off The start offset in array b at which the data is written.
+     * @param len The maximum number of bytes read
+     * @return The total number of bytes read into the buffer, 
+     *         or -1 if there is no more data because the end of this file has been reached. 
+     * @throws IOException If I/O error occurs
+     * @throws NullPointerException If <code>b</code> is <code>null</code>
+     * @throws MalformedBgzipDataException If compressed data is not valid BGZF format
+     */
     public int read(byte[] b, int off, int len) throws IOException, MalformedBgzipDataException {
         if (b == null) {
             throw new NullPointerException();
@@ -129,7 +285,7 @@ public class RandomAccessBgzFile implements AutoCloseable, Cloneable {
             
             // Read compresed data from block
             byte[] compressed = new byte[block.getDataLength()];
-            channel.position(block.getDataOffset());
+            channel.position(basePosition + block.getDataOffset());
             int n = channel.read(ByteBuffer.wrap(compressed));
             if (n != block.getDataLength()) {
                 throw new MalformedBgzipDataException("Wrong compressed data block: block data incomplete to read");
@@ -248,10 +404,10 @@ public class RandomAccessBgzFile implements AutoCloseable, Cloneable {
         }
         builder.dataLength(dataLength);
         
-        long dataOffset = channel.position();
+        long dataOffset = channel.position() - basePosition;
         builder.dataOffset(dataOffset);
         // Skip data block
-        channel.position(dataOffset + dataLength + 4);  // sizeof(CRC32) = 4
+        channel.position(basePosition + dataOffset + dataLength + 4);  // sizeof(CRC32) = 4
         
         // ISIZE
         bb = ByteBuffer.allocateDirect(4);
